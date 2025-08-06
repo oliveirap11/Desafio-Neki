ffa7c4dccf954791c859ff755312a32c20a4a9f9
oliveirap11 <patricks9612@gmail.com>
2025-08-06 18:55:36 -0300

    primeiro commit

---
+++ BackEnd/Neki/src/main/java/desafio/Neki/NekiApplication.java
@@ -0,0 +1,13 @@
+package desafio.Neki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekiApplication.class, args);
+	}
+
+}
